e42749b698e2287d34247a2186e5e7576f07add6
Yonal Alexis Londono <yolondono@personalsoft.com.co>
2019-12-23 21:22:07 -0500

    se agregan los archivos del commit inicial

---
+++ Cuackable.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface Cuackable {
+
+    // todos lo metodos abstractos son publicos
+    // las interfaces por defecto son abstractos
+    //public abstract void toCuack();
+
+    void toCuack();
+
+}
---
+++ Flyable.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public interface Flyable {
+    //tambbien se puede oviar
+    // double distancia = 0;
+
+    // TODAS LAS CONSTANTES VAN EN MAYUSCULA
+    public static final double DISTANCIA = 0;
+
+    void doFly();
+
+}
